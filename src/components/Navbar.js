bd626fbb19ba78f8712526b359fd73e838fb2436
Bolo Chen <bolosstuff12@gmail.com>
2022-12-09 14:27:58 +0700

    change layout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,8 +13,16 @@ const Navbar = () => {
     }
   }
   return (
-    <div className='border-solid border-2 border-sky-500 bg-gray-400 py-3 h-16'>
-        <button onClick={Logout} className='float-right p-3 text-white bg-green-500 rounded-xl '>Logout</button>
+    <div class="bg-white border-gray-200 dark:bg-gray-900">
+    <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5">
+        <a href="/dashboard" class="flex items-center">
+            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Home</span>
+        </a>
+        <div class="flex items-center">
+            <a class="mr-6 text-sm font-medium text-gray-500 dark:text-white hover:underline">Options</a>
+            <button onClick={Logout} className='float-right p-3 text-white bg-green-500 rounded-xl '>Logout</button>
+        </div>
+    </div>
     </div>
   )
 }
